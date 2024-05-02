a15fe753418562807fe844f71f9ded46a1708232
Puajshree Ghosh <160346094+PujashreeG@users.noreply.github.com>
2024-05-02 14:26:24 +0530

    Add files via upload

---
+++ JS Basics Boolean Solution.js
@@ -0,0 +1,83 @@
+//Boolean
+
+//1. Here, we declare the variable isTrue. But, it has no value yet. Assign a boolean value to it and run the code.
+
+// let isTrue;
+
+// console.log(isTrue); //undefined
+
+//Sol
+
+// let isTrue;
+// isTrue = true;
+// console.log(isTrue);
+
+//falsy values------------
+
+// false
+// null
+// undefined
+// 0
+// -0
+// NaN
+// An empty string ('')
+
+//2. What will be the output?
+
+// const price = 10;
+// console.log(Boolean(price)); //true
+
+//3. Here, we declare the variable num and assign it the value 5. We also declare the variable bool which we assign the boolean representation of num. Extend the code such that the console.log() statement logs false.
+
+// let num = 5;
+
+// const bool = Boolean(num);
+// console.log(bool);
+
+//Solution
+// let num = 5;
+// num = 0;
+// const bool = Boolean(num);
+// console.log(bool);
+
+//4. True or False?
+
+// const num = 10;
+// num is falsy.
+
+//Ans. False
+
+//5. True or False?
+
+// Every single value in JavaScript can be transformed to a boolean value.
+
+//Ans. True
+
+//6.What will be the output?
+
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean(-1));
+
+//Ans true false true
+
+//7. What will be the output?
+
+// const name = "james";
+// console.log(Boolean(name));
+
+//Ans. True
+
+//8. What will be the output?
+
+// const price = 10;
+// console.log(Boolean(price));
+
+//Ans. True
+
+//9. What will be the output?
+
+// let isTrue = true;
+// console.log(isTrue);
+
+//Ans. True
